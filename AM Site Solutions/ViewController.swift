70f61ca0dca3703fd336d97230fd929bfaeb8e4b
warrenhharding <warrenhharding@gmail.com>
2024-09-03 18:52:19 +0100

    Initial Commit

---
+++ AM Site Solutions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AM Site Solutions
+//
+//  Created by Warren Harding on 03/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
